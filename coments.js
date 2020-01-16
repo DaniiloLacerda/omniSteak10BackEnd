4f5f0a34eac0732386df0527185dc0424b6a0ad6
Mattax <danyllo.lacerda@hotmail.com>
2020-01-15 23:33:27 -0200

    first commit

---
+++ coments.js
@@ -0,0 +1,9 @@
+
+//Métodos HTTP: GET POST, PUT, DELETE
+// Query Params: req.query (Filtros, ordenação, paginação, ...)
+// Route Params: request.params (Identificar um recurso na alteração ou remoção)
+// Body: request.body (Dados para criação ou alteração de um registro)
+
+//Mongo DB  (Não Relacional)
+
+// index, store, show, update, destroy
